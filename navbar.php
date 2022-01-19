8f944606e7db182a46cb5018455034c6c41dd11b
Aliasgar palasara <89629548+aliasgarpalasara@users.noreply.github.com>
2022-01-19 16:24:28 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,43 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-lg navbar-light"style="background: linear-gradient(to right, transparent 16%, rgba(125,185,232,0) 0%, rgba(0,0,0,1) 100%);">
+      <img src="image/logo.jpg"  class="logo" height="50px";  >
+      <a class="navbar-brand" href="index.php" >SPARKS BANK</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+			
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color: rgb(251 243 243 / 50%);
+    font-size: 13px;
+    font-weight: 700;
+    border: 1px solid rgb(6 6 6 / 50%);
+	letter-spacing: 0.5px;">HOME</a>
+              </li>
+              
+              <li class="nav-item">
+                <a class="nav-link" href="create-user.php"  style="color: rgb(251 243 243 / 50%);
+    font-size: 13px;
+    font-weight: 700;
+    border: 1px solid rgb(6 6 6 / 50%);
+	letter-spacing: 0.5px;" >CREATE USER</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php"  style="color: rgb(251 243 243 / 50%);
+    font-size: 13px;
+    font-weight: 700;
+    border: 1px solid rgb(6 6 6 / 50%);
+	letter-spacing: 0.5px;">TRANSFER MONEY</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php"  style="color: rgb(251 243 243 / 50%);
+    font-size: 13px;
+    font-weight: 700;
+    border-radios:10px;
+    border: 1px solid rgb(6 6 6 / 50%);
+	letter-spacing: 0.5px; ">TRANSACTION HISTORY</a>
+              </li>
+          </div>
+       </nav>
